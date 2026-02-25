51fc796314fe73df55f9cd0b4cdc32ae08511982
srushti <srushtitandel4546@gmail.com>
2026-02-25 20:40:02 +0530

    git push origin mainadded practical 4

--- AlphabetPyramid.java
+++ AlphabetPyramid.java
@@ -2,21 +2,35 @@ public class AlphabetPyramid {
 
     public static void main(String[] args) {
 
-        
+        int rows = 5;
 
-        int n = 5;
+        // ----- Star Pattern -----
+        for (int i = 1; i <= rows; i++) {
 
-        for (int i = n; i >= 1; i--) {
-
-            for (int s = 0; s < n - i; s++) {
+            for (int s = 1; s <= rows - i; s++) {
                 System.out.print(" ");
             }
 
-            char ch = 'A';
-
             for (int j = 1; j <= i; j++) {
-                System.out.print(ch + " ");
-                ch++;
+                System.out.print("*");
+                if (j < i) System.out.print(" ");
+            }
+
+            System.out.println();
+        }
+
+        // ❌ Removed the extra println here
+
+        // ----- Alphabet Pattern -----
+        for (int i = 0; i < rows; i++) {
+
+            for (int s = 0; s < i; s++) {
+                System.out.print(" ");
+            }
+
+            for (int j = 0; j < rows - i; j++) {
+                System.out.print((char) ('A' + j));
+                if (j < rows - i - 1) System.out.print(" ");
             }
 
             System.out.println();
--- NumberPattern.java
+++ NumberPattern.java
@@ -1,19 +1,29 @@
 public class NumberPattern {
-
     public static void main(String[] args) {
 
-        System.out.println("Number Pattern:");
+       
+        int rows = 6;
+
+        for (int i = rows; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
 
         int num = 1;
+        int maxRows = 5;
 
-        for (int i = 1; i <= 5; i++) {
+        for (int i = 1; i <= maxRows; i++) {
             for (int j = 1; j <= i; j++) {
 
                 System.out.print(num + " ");
                 num++;
 
-                if (num == 10)
+                // Reset to 1 after 9
+                if (num > 9) {
                     num = 1;
+                }
             }
             System.out.println();
         }
